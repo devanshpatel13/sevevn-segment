e37403619b41ce0d122648dbd2348241ce382a3e
plutusdev <devansh.patel@plutustec.com>
2023-02-17 13:00:33 +0530

    latest update 17-feb-1:00

--- 1.py
+++ 1.py
@@ -372,67 +372,194 @@ import numpy as np
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
+#
+# import cv2  # OpenCV Library
+#
+# # Image to detect shapes on below
+# image = cv2.imread("SaleDigit.jpg")
+#
+# gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Converting to gray image
+#
+# # Setting threshold value to get new image (In simpler terms: this function checks every pixel, and depending on how
+# # dark the pixel is, the threshold value will convert the pixel to either black or white (0 or 1)).
+# _, thresh_image = cv2.threshold(gray_image, 220, 255, cv2.THRESH_BINARY)
+#
+# # Retrieving outer-edge coordinates in the new threshold image
+# contours, hierarchy = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#
+# # Iterating through each contour to retrieve coordinates of each shape
+# for i, contour in enumerate(contours):
+#     if i == 0:
+#         continue
+#
+#     # The 2 lines below this comment will approximate the shape we want. The reason being that in certain cases the
+#     # shape we want might have flaws or might be imperfect, and so, for example, if we have a rectangle with a
+#     # small piece missing, the program will still count it as a rectangle. The epsilon value will specify the
+#     # precision in which we approximate our shape.
+#     epsilon = 0.01 * cv2.arcLength(contour, True)
+#     approx = cv2.approxPolyDP(contour, epsilon, True)
+#
+#     # Drawing the outer-edges onto the image
+#     cv2.drawContours(image, contour, 0, (0, 0, 0), 4)
+#
+#     # Retrieving coordinates of the contour so that we can put text over the shape.
+#     x, y, w, h = cv2.boundingRect(approx)
+#     x_mid = int(x + (w / 3))  # This is an estimation of where the middle of the shape is in terms of the x-axis.
+#     y_mid = int(y + (h / 1.5))  # This is an estimation of where the middle of the shape is in terms of the y-axis.
+#
+#     # Setting some variables which will be used to display text on the final image
+#     coords = (x_mid, y_mid)
+#     colour = (0, 0, 0)
+#     font = cv2.FONT_HERSHEY_DUPLEX
+#
+#     # This is the part where we actually guess which shape we have detected. The program will look at the amount of edges
+#     # the contour/shape has, and then based on that result the program will guess the shape (for example, if it has 3 edges
+#     # then the chances that the shape is a triangle are very good.)
+#     #
+#     # You can add more shapes if you want by checking more lenghts, but for the simplicity of this tutorial program I
+#     # have decided to only detect 5 shapes.
+#     if len(approx) == 3:
+#         cv2.putText(image, "Triangle", coords, font, 1, colour, 1)  # Text on the image
+#     elif len(approx) == 4:
+#         cv2.putText(image, "Quadrilateral", coords, font, 1, colour, 1)
+#     elif len(approx) == 5:
+#         cv2.putText(image, "Pentagon", coords, font, 1, colour, 1)
+#     elif len(approx) == 6:
+#         cv2.putText(image, "Hexagon", coords, font, 1, colour, 1)
+#     else:
+#         # If the length is not any of the above, we will guess the shape/contour to be a circle.
+#         cv2.putText(image, "Circle", coords, font, 1, colour, 1)
+#
+# # Displaying the image with the detected shapes onto the screen
+# cv2.imshow("shapes_detected", image)
+# cv2.waitKey(0)
 
-import cv2  # OpenCV Library
-
-# Image to detect shapes on below
-image = cv2.imread("SaleDigit.jpg")
-
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Converting to gray image
-
-# Setting threshold value to get new image (In simpler terms: this function checks every pixel, and depending on how
-# dark the pixel is, the threshold value will convert the pixel to either black or white (0 or 1)).
-_, thresh_image = cv2.threshold(gray_image, 220, 255, cv2.THRESH_BINARY)
-
-# Retrieving outer-edge coordinates in the new threshold image
-contours, hierarchy = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-# Iterating through each contour to retrieve coordinates of each shape
-for i, contour in enumerate(contours):
-    if i == 0:
-        continue
-
-    # The 2 lines below this comment will approximate the shape we want. The reason being that in certain cases the
-    # shape we want might have flaws or might be imperfect, and so, for example, if we have a rectangle with a
-    # small piece missing, the program will still count it as a rectangle. The epsilon value will specify the
-    # precision in which we approximate our shape.
-    epsilon = 0.01 * cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, epsilon, True)
-
-    # Drawing the outer-edges onto the image
-    cv2.drawContours(image, contour, 0, (0, 0, 0), 4)
-
-    # Retrieving coordinates of the contour so that we can put text over the shape.
-    x, y, w, h = cv2.boundingRect(approx)
-    x_mid = int(x + (w / 3))  # This is an estimation of where the middle of the shape is in terms of the x-axis.
-    y_mid = int(y + (h / 1.5))  # This is an estimation of where the middle of the shape is in terms of the y-axis.
-
-    # Setting some variables which will be used to display text on the final image
-    coords = (x_mid, y_mid)
-    colour = (0, 0, 0)
-    font = cv2.FONT_HERSHEY_DUPLEX
-
-    # This is the part where we actually guess which shape we have detected. The program will look at the amount of edges
-    # the contour/shape has, and then based on that result the program will guess the shape (for example, if it has 3 edges
-    # then the chances that the shape is a triangle are very good.)
-    #
-    # You can add more shapes if you want by checking more lenghts, but for the simplicity of this tutorial program I
-    # have decided to only detect 5 shapes.
-    if len(approx) == 3:
-        cv2.putText(image, "Triangle", coords, font, 1, colour, 1)  # Text on the image
-    elif len(approx) == 4:
-        cv2.putText(image, "Quadrilateral", coords, font, 1, colour, 1)
-    elif len(approx) == 5:
-        cv2.putText(image, "Pentagon", coords, font, 1, colour, 1)
-    elif len(approx) == 6:
-        cv2.putText(image, "Hexagon", coords, font, 1, colour, 1)
-    else:
-        # If the length is not any of the above, we will guess the shape/contour to be a circle.
-        cv2.putText(image, "Circle", coords, font, 1, colour, 1)
-
-# Displaying the image with the detected shapes onto the screen
-cv2.imshow("shapes_detected", image)
-cv2.waitKey(0)
 
 
 
+
+
+
+
+import cv2
+import numpy as np
+from PIL import Image
+img = cv2.imread("image3.png");
+# img = cv2.imread("green-font.png");
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("green-font-gray.png", gray)
+
+blur =  cv2.GaussianBlur(gray, (7,7), 0)
+cv2.imwrite("green-font-blur.png", blur)
+
+img = cv2.threshold(blur, 0,255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+cv2.imwrite("green-font-thresh.png", img)
+row = len(img)
+col = len(img[0])
+
+
+
+
+
+def find_first_black_row(row, col):
+    for first_black in range(0 , row-1):
+        for first_col_black in range(0, col-1):
+            if int(img[first_black][first_col_black]) == 0:
+                # import pdb; pdb.set_trace()
+                print("this is black point", first_black, first_col_black)
+                return first_black
+    return find_first_black_row()
+
+def find_last_black_row(row, col):
+    for last_row_black in range(row-1, 0, -1):
+        for last_col_black in range(0, col-1):
+            if int(img[last_row_black][last_col_black]) == 0:
+                # import pdb; pdb.set_trace()
+                print("this is black point", last_row_black, last_col_black)
+                return last_row_black
+    return find_last_black_row()
+
+
+
+def afterfirst_ending_point(i, j, aa):
+    fix_row = row - 1
+    # import pdb; pdb.set_trace()
+    for j in range(j - 1, 0, -1):
+        for i in range(fix_row, 0, -1):
+            if int(img[i][j]) == 0:
+                if (j + 1) in aa:
+                    return i, j
+                print("this is start point")
+    return afterfirst_ending_point()
+
+def start_point(i, j, point):
+    fix_row = row - 1
+    for j in range(j-1, 0, -1):
+        for i in range(fix_row, 0, -1):
+            if int(img[i][j]) == 0:
+                if (j-1) in point:
+                    return i, j
+                print("this is start point")
+    return start_point()
+def end_point(rows, col):
+    fix_row = row - 1
+    check_point = False
+    point = []
+    white_pixel_check =[]
+    for j in range(col, 0, -1):
+        for i in range(fix_row, 0, -1):
+
+            white_pixel_check.append((int(img[i][j])))
+            if int(img[i][j]) == 0:
+                break
+        white_pixel_check.sort()
+        if white_pixel_check[0] == 255:
+            point.append(j)
+            white_pixel_check.clear()
+        else:
+            white_pixel_check.clear()
+
+    return point
+
+row_cut_start_point = find_first_black_row(row, col)
+row_cut_end_point = find_last_black_row(row, col)
+after_first_box_ending_point =[]
+check_start_point =[]
+flage = True
+start_point1 =[]
+crop_name = 0
+for j in range(col, 0, -1):
+    for i in range(row, 0, -1):
+        if int(img[i - 1][j - 1]) == 0:
+            aa = list(end_point(i-1, j - 1))
+            start_point1 = list(start_point(i-1, j-1,aa))
+            print("this is check point...........!")
+            if flage:
+                # import pdb; pdb.set_trace()
+                cv2.rectangle(img, (start_point1[1], row_cut_start_point), (j, row_cut_end_point), (30, 250, 12), 1)
+                cv2.imwrite("green-font-bbox.png", img)
+                img = Image.open('green-font-bbox.png')
+                crop_img = img.crop((start_point1[1] + 1, row_cut_start_point, j, row_cut_end_point))
+                crop_img.save("last_image.jpg")
+                crop_img.show()
+                img = np.array(img)
+                after_first_box_ending_point.append(start_point1[1] - 1)
+                start_point1.clear()
+                flage = False
+            else:
+                crop_name += 1
+                ending_point = list(afterfirst_ending_point(i, j, aa))
+                # import pdb;pdb.set_trace()
+                cv2.rectangle(img, (start_point1[1], row_cut_start_point), (ending_point[1], row_cut_end_point), (30, 250, 12),1)
+                cv2.imwrite("green-font-bbox.png", img)
+                img = Image.open('green-font-bbox.png')
+                crop_img = img.crop((start_point1[1] + 1, row_cut_start_point, ending_point[1] + 1,row_cut_end_point))
+                crop_img.save(f"last_image{str(crop_name)}.jpg")
+                crop_img.show()
+                img = np.array(img)
+                after_first_box_ending_point.clear()
+                print(after_first_box_ending_point, "after_first_box_ending_point")
+                after_first_box_ending_point.append(start_point1[1] - 1)
+                start_point1.clear()
+                ending_point.clear()
--- practice2.py
+++ practice2.py
@@ -1,3 +1,165 @@
+# import cv2
+# import numpy as np
+# from PIL import Image
+# img = cv2.imread("image2.png");
+# # img = cv2.imread("green-font.png");
+#
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# cv2.imwrite("green-font-gray.png", gray)
+#
+# blur =  cv2.GaussianBlur(gray, (7,7), 0)
+# cv2.imwrite("green-font-blur.png", blur)
+#
+# img = cv2.threshold(blur, 0,255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+# cv2.imwrite("green-font-thresh.png", img)
+# # import pdb; pdb.set_trace()
+# row = len(img)
+# col = len(img[0])
+#
+#
+# def find_first_black_row(row, col):
+#     for first_black in range(0 , row-1):
+#         for first_col_black in range(0, col-1):
+#             if int(img[first_black][first_col_black]) == 0:
+#                 # import pdb; pdb.set_trace()
+#                 print("this is black point", first_black, first_col_black)
+#                 return first_black
+#     return find_first_black_row()
+#
+# def find_last_black_row(row, col):
+#     for last_row_black in range(row-1, 0, -1):
+#         for last_col_black in range(0, col-1):
+#             if int(img[last_row_black][last_col_black]) == 0:
+#                 # import pdb; pdb.set_trace()
+#                 print("this is black point", last_row_black, last_col_black)
+#                 return last_row_black
+#     return find_last_black_row()
+#
+# def afterfirst_ending_point(i, j, aa):
+#     fix_row = row - 1
+#     # import pdb; pdb.set_trace()
+#     for j in range(j - 1, 0, -1):
+#         for i in range(fix_row, 0, -1):
+#             if int(img[i][j]) == 0:
+#                 if (j + 1) in aa:
+#                     return i, j
+#                     # import pdb; pdb.set_trace()
+#                 # ...
+#                 # import pdb; pdb.set_trace()
+#                 print("this is start point")
+#     return afterfirst_ending_point()
+#
+# def start_point(i, j, point):
+#     fix_row = row - 1
+#     # import pdb; pdb.set_trace()
+#     print("this is j check point .................! line 59", i, j )
+#     for j in range(j-1, 0, -1):
+#         for i in range(fix_row, 0, -1):
+#             if int(img[i][j]) == 0:
+#                 if (j-1) in point:
+#                     print("this is j check point .................! line 64", i, j)
+#
+#                     import pdb;pdb.set_trace()
+#                     return i, j
+#                 # import pdb; pdb.set_trace()
+#                 # ...
+#                 # import pdb; pdb.set_trace()
+#                 print("this is start point")
+#     return start_point()
+# def end_point(rows, col):
+#     fix_row = row - 1
+#     check_point = False
+#     point = []
+#     white_pixel_check =[]
+#     # import pdb; pdb.set_trace()
+#     for j in range(col, 0, -1):
+#         for i in range(fix_row, 0, -1):
+#
+#             white_pixel_check.append((int(img[i][j])))
+#             if int(img[i][j]) == 0:
+#                 break
+#         white_pixel_check.sort()
+#         if white_pixel_check[0] == 255:
+#             point.append(j)
+#             white_pixel_check.clear()
+#         else:
+#             white_pixel_check.clear()
+#         # import pdb; pdb.set_trace()
+#
+#     return point
+#
+# row_cut_start_point = find_first_black_row(row, col)
+# row_cut_end_point = find_last_black_row(row, col)
+# # import pdb;pdb.set_trace()
+# after_first_box_ending_point =[]
+# check_start_point =[]
+# flage = True
+# start_point1 =[]
+# crop_name = 0
+# for j in range(col, 0, -1):
+#     for i in range(row, 0, -1):
+#         if int(img[i - 1][j - 1]) == 0:
+#             aa = list(end_point(i-1, j - 1))
+#             start_point1 = list(start_point(i-1, j-1,aa))
+#             if flage:
+#                 cv2.rectangle(img, (start_point1[1], row_cut_start_point), (j, row-1), (30, 250, 12), 1)
+#                 cv2.imwrite("green-font-bbox.png", img)
+#                 img = Image.open('green-font-bbox.png')
+#                 crop_img = img.crop((start_point1[1] + 1, row_cut_start_point, j, row_cut_end_point))
+#                 crop_img.save("last_image.jpg")
+#                 crop_img.show()
+#                 img = np.array(img)
+#                 after_first_box_ending_point.append(start_point1[1] - 1)
+#                 start_point1.clear()
+#                 flage = False
+#             else:
+#                 crop_name += 1
+#                 ending_point = list(afterfirst_ending_point(i, j, aa))
+#                 cv2.rectangle(img, (start_point1[1], row_cut_start_point), (ending_point[1], row), (30, 250, 12),1)
+#                 img = Image.open('green-font-bbox.png')
+#                 crop_img = img.crop((start_point1[1] + 1, row_cut_start_point, ending_point[1] + 1, row_cut_end_point))
+#                 crop_img.save(f"last_image{str(crop_name)}.jpg")
+#                 crop_img.show()
+#                 img = np.array(img)
+#                 after_first_box_ending_point.clear()
+#                 print(after_first_box_ending_point, "after_first_box_ending_point")
+#                 after_first_box_ending_point.append(start_point1[1] - 1)
+#                 start_point1.clear()
+#                 ending_point.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import cv2
 import numpy as np
 from PIL import Image
@@ -12,30 +174,30 @@ cv2.imwrite("green-font-blur.png", blur)
 
 img = cv2.threshold(blur, 0,255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 cv2.imwrite("green-font-thresh.png", img)
-# import pdb; pdb.set_trace()
 row = len(img)
 col = len(img[0])
 
 
+
+
+
 def find_first_black_row(row, col):
-    for i in range(0 , row-1):
-        for j in range(0, col-1):
-            if int(img[i][j]) == 0:
+    for first_black in range(0 , row-1):
+        for first_col_black in range(0, col-1):
+            if int(img[first_black][first_col_black]) == 0:
                 # import pdb; pdb.set_trace()
-                print("this is black point", i, j)
-                return i
+                print("this is black point", first_black, first_col_black)
+                return first_black
     return find_first_black_row()
 
-
 def find_last_black_row(row, col):
-    for i in range(row-1, 0, -1):
-        for j in range(0, col-1):
-            if int(img[i][j]) == 0:
+    for last_row_black in range(row-1, 0, -1):
+        for last_col_black in range(0, col-1):
+            if int(img[last_row_black][last_col_black]) == 0:
                 # import pdb; pdb.set_trace()
-                print("this is black point", i, j)
-                return i
+                print("this is black point", last_row_black, last_col_black)
+                return last_row_black
     return find_last_black_row()
-
 
 
 
@@ -47,23 +209,16 @@ def afterfirst_ending_point(i, j, aa):
             if int(img[i][j]) == 0:
                 if (j + 1) in aa:
                     return i, j
-                # import pdb; pdb.set_trace()
-                # ...
-                # import pdb; pdb.set_trace()
                 print("this is start point")
     return afterfirst_ending_point()
 
 def start_point(i, j, point):
     fix_row = row - 1
-    # import pdb; pdb.set_trace()
     for j in range(j-1, 0, -1):
         for i in range(fix_row, 0, -1):
             if int(img[i][j]) == 0:
                 if (j-1) in point:
                     return i, j
-                # import pdb; pdb.set_trace()
-                # ...
-                # import pdb; pdb.set_trace()
                 print("this is start point")
     return start_point()
 def end_point(rows, col):
@@ -71,30 +226,23 @@ def end_point(rows, col):
     check_point = False
     point = []
     white_pixel_check =[]
-    # import pdb; pdb.set_trace()
     for j in range(col, 0, -1):
         for i in range(fix_row, 0, -1):
 
             white_pixel_check.append((int(img[i][j])))
             if int(img[i][j]) == 0:
                 break
-            # if int(img[i][j]) == 0:
-            #     point.append(j)
-            #     check_point = True
-            #     break
         white_pixel_check.sort()
         if white_pixel_check[0] == 255:
             point.append(j)
             white_pixel_check.clear()
         else:
             white_pixel_check.clear()
-        # import pdb; pdb.set_trace()
 
     return point
 
 row_cut_start_point = find_first_black_row(row, col)
 row_cut_end_point = find_last_black_row(row, col)
-# import pdb;pdb.set_trace()
 after_first_box_ending_point =[]
 check_start_point =[]
 flage = True
@@ -102,21 +250,13 @@ start_point1 =[]
 crop_name = 0
 for j in range(col, 0, -1):
     for i in range(row, 0, -1):
-        # import pdb; pdb.set_trace()
         if int(img[i - 1][j - 1]) == 0:
-            # if int(img[i-2][j-2]) == 255:
-
-            # row-=1
-            # col-=1
-            # import pdb;pdb.set_trace()
             aa = list(end_point(i-1, j - 1))
             start_point1 = list(start_point(i-1, j-1,aa))
-
-            # import pdb;pdb.set_trace()
             print("this is check point...........!")
             if flage:
                 # import pdb; pdb.set_trace()
-                cv2.rectangle(img, (start_point1[1], row_cut_start_point), (j, row-1), (30, 250, 12), 0)
+                cv2.rectangle(img, (start_point1[1], row_cut_start_point), (j, row_cut_end_point), (30, 250, 12), 1)
                 cv2.imwrite("green-font-bbox.png", img)
                 img = Image.open('green-font-bbox.png')
                 crop_img = img.crop((start_point1[1] + 1, row_cut_start_point, j, row_cut_end_point))
@@ -130,17 +270,13 @@ for j in range(col, 0, -1):
                 crop_name += 1
                 ending_point = list(afterfirst_ending_point(i, j, aa))
                 # import pdb;pdb.set_trace()
-                # cv2.rectangle(img, (start_point1[1], row_cut_start_point), (ending_point[1], row), (30, 250, 12),0)
-                # cv2.rectangle(img, (start_point1[1], 0), (after_first_box_ending_point[0], row), (30, 250, 12),1)
-                # cv2.imwrite("green-font-bbox.png", img)
+                cv2.rectangle(img, (start_point1[1], row_cut_start_point), (ending_point[1], row_cut_end_point), (30, 250, 12),1)
+                cv2.imwrite("green-font-bbox.png", img)
                 img = Image.open('green-font-bbox.png')
-                # import pdb; pdb.set_trace()
-                crop_img = img.crop((start_point1[1] + 1, row_cut_start_point, ending_point[1] + 1, row_cut_end_point))
-                # crop_img = img.crop((start_point1[1] + 1, 1, after_first_box_ending_point[0] + 1, row + 1))
+                crop_img = img.crop((start_point1[1] + 1, row_cut_start_point, ending_point[1] + 1,row_cut_end_point))
                 crop_img.save(f"last_image{str(crop_name)}.jpg")
                 crop_img.show()
                 img = np.array(img)
-                # img = img.crop((after_first_box_ending_point[0], 0, j, row))
                 after_first_box_ending_point.clear()
                 print(after_first_box_ending_point, "after_first_box_ending_point")
                 after_first_box_ending_point.append(start_point1[1] - 1)
